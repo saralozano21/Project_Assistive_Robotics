6de122cc6f1682b0002ec24a84ce5753c2cc3447
unknown <consuelo.brito@fhios.local>
2025-09-26 22:49:23 +0200

    wave_sara

--- src/python_scripts/sara Assistive_hand_SW_HW_sockets copy.py
+++ src/python_scripts/sara Assistive_hand_SW_HW_sockets copy.py
@@ -3,25 +3,25 @@ import time
 import socket
 import tkinter as tk
 from tkinter import messagebox
-# from math import radians, degrees, pi
+from math import radians, degrees, pi
 import math
 from robodk.robolink import *
 from robodk.robomath import *
 
 # Load RoboDK project from relative path
-relative_path = "src/roboDK/Assistive_UR5e.rdk"
+relative_path = "Assistive_UR5e.rdk"
 absolute_path = os.path.abspath(relative_path)
 RDK = Robolink()
-RDK.AddFile(absolute_path)
+# RDK.AddFile(absolute_path)
 
 # Robot setup
 robot = RDK.Item("UR5e")
 base = RDK.Item("UR5e Base")
 tool = RDK.Item('Hand')
 Init_target = RDK.Item('Init')
-wave_start = RDK.Item('wave_start')
-wave_left = RDK.Item('wave_left')
-wave_right = RDK.Item('wave_right')
+wave_start_target = RDK.Item('wave_start')
+wave_left_target = RDK.Item('wave_left')
+wave_right_target = RDK.Item('wave_right')
 
 robot.setPoseFrame(base)
 robot.setPoseTool(tool)
@@ -43,15 +43,15 @@ deg_init = [-57.835746, -65.391528, -107.167117, 172.558645, -57.835746, 0.00000
 rad_init = [math.radians(j) for j in deg_init]
 movej_init = f"movej({rad_init},{accel_mss},{speed_ms},{timel},0.0000)"
 
-deg_start = [-69.512703, -83.328690, -135.275772, 311.041688, -90.910275, -20.467938]
+deg_start = [-61.661349, -60.826168, -126.275295, 227.862971, -67.780209, -18.054745]
 rad_start =[math.radians(j) for j in deg_start]
 movel_wave_start = f"movel({rad_start},{accel_mss},{speed_ms},{timel},0.000)"
 
-deg_left = [-62.563099, -60.494396, -132.897239, 251.324832, -74.590173, -22.983841]
+deg_left = [-72.331751, -93.354688, -127.409127, 325.269567, -94.561656, -17.087400]
 rad_left = [math.radians(j) for j in deg_left]
 movel_wave_left = f"movel({rad_left},{accel_mss},{speed_ms},{timel/2},0.000)"
 
-deg_right = [-70.611710, -88.334644, -133.375880, 321.311138, -93.359063, -19.106131]
+deg_right = [-63.680100, -63.491305, -130.156774, 245.118521, -72.874121, -20.294912]
 rad_right = [math.radians(j) for j in deg_right]
 movel_wave_right = f"movel({rad_right},{accel_mss},{speed_ms},{timel/2},0.000)"
 
@@ -74,22 +74,25 @@ def send_ur_script(command):
 def Init():
     print("Init")
     robot.MoveL(Init_target, True)
-    print("Init_target REACHED")
+    print("Init_target REACHED") 
     if robot_is_connected:
         print("Init REAL UR5e")
         send_ur_script(set_tcp)
         send_ur_script(movej_init)
-        time.sleep(timej)
-    else:
-        print("UR5e not connected. Simulation only.")
 
 def Wave():
-    # Simulación en RoboDK
-    robot.MoveL(wave_start, True)
+    robot.MoveL(wave_start_target, True)
     for _ in range(3):
-        robot.MoveL(wave_left, True)
-        robot.MoveL(wave_right, True)
-    robot.MoveL(wave_start, True)
+        robot.MoveL(wave_left_target, True)
+        robot.MoveL(wave_right_target, True)
+    robot.MoveL(wave_start_target, True)
+
+    if robot_is_connected:
+        send_ur_script(set_tcp)
+        send_ur_script(movel_wave_left)
+        send_ur_script(movel_wave_right)
+        send_ur_script(movel_wave_left)
+
 
 # Confirmation dialog to close RoboDK
 def confirm_close():
